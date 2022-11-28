203c03725729b98f3f8e937187179c583bb623f4
Humzas <humzas@umich.edu>
2022-11-28 16:56:34 -0500

    fixed buttons

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,16 @@
 var linkedIn = document.querySelector("#liBtn");
 var gitHub = document.querySelector("#ghBtn");
+
+gitHub.addEventListener("click", function() {
+    window.open(
+        "https://github.com/HumzaShaukat",
+        "_blank"
+    )
+});
+
+linkedIn.addEventListener("click",function() {
+    window.open(
+        "https://www.linkedin.com/in/humza-shaukat/",
+        "_blank"
+    )
+});
